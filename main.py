b7ad08db06f77e51ac4ef8b9fbbb62e11d432a52
PoyrazYare156 <101834922+PoyrazYare156@users.noreply.github.com>
2025-07-16 01:18:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import httpx
+import cloudscraper
 
 app = FastAPI()
 
